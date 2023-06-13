6d929a832668bc43a307dbcda87374803462090f
Mohamad1375 <Mohammadali.fallahzade@gmail.com>
2023-06-13 16:18:54 +0330

    import your own files

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// const add = require('./utils.js') 
-
-
-// const sum = add(2,2);
-
-// console.log(sum);
-
+const validator = require('validator')
 const getNotes = require('./notes')
 
-const msg = getNotes();
 
+
+
+const msg = getNotes();
 console.log(msg);
+
+console.log(validator.isEmail('mafkk@yahoo.com'));
